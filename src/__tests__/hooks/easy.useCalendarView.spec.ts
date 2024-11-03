2bdaa5bf8593f771cd8c2eaec1e78b37c3e1f5d4
ginttone <alicelikesbab@gmail.com>
2024-11-03 23:38:40 +0900

    feat: hooks>easy test

--- src/__tests__/hooks/easy.useCalendarView.spec.ts
+++ src/__tests__/hooks/easy.useCalendarView.spec.ts
@@ -1,24 +1,121 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
 import { useCalendarView } from '../../hooks/useCalendarView.ts';
 import { assertDate } from '../utils.ts';
 
 describe('초기 상태', () => {
-  it('view는 "month"이어야 한다', () => {});
+  beforeAll(() => {
+    //fake timer를 사용해서 테스트 데이터의 오늘 날짜를 '2024-10-01'고정한다.
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2024-10-01'));
+  });
 
-  it('currentDate는 오늘 날짜인 "2024-10-01"이어야 한다', () => {});
+  it('view는 "month"이어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    expect(result.current.view).toBe('month');
+  });
 
-  it('holidays는 10월 휴일인 개천절, 한글날이 지정되어 있어야 한다', () => {});
+  it('currentDate는 오늘 날짜인 "2024-10-01"이어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+
+    const date = new Date('2024-10-01');
+    // const currentDate = date.toLocaleDateString('ko-KR');
+    // const formattingDate = date.toISOString().split('T')[0];
+    // const currentDate = result.current.currentDate.toISOString().split('T')[0];
+
+    //오늘 날짜로 테스트 데이터가 들어가야 한다.
+    // expect(currentDate).toBe(formattingDate);
+    assertDate(result.current.currentDate, date);
+  });
+
+  it('holidays는 10월 휴일인 개천절, 한글날이 지정되어 있어야 한다', async () => {
+    const { result } = renderHook(() => useCalendarView());
+
+    expect(result.current.holidays).toMatchObject({
+      '2024-10-03': '개천절',
+      '2024-10-09': '한글날',
+    });
+  });
 });
 
-it("view를 'week'으로 변경 시 적절하게 반영된다", () => {});
+it("view를 'week'으로 변경 시 적절하게 반영된다", () => {
+  const { result } = renderHook(() => useCalendarView());
 
-it("주간 뷰에서 다음으로 navigate시 7일 후 '2024-10-08' 날짜로 지정이 된다", () => {});
+  act(() => {
+    result.current.setView('week');
+  });
 
-it("주간 뷰에서 이전으로 navigate시 7일 후 '2024-09-24' 날짜로 지정이 된다", () => {});
+  expect(result.current.view).toBe('week');
+});
 
-it("월간 뷰에서 다음으로 navigate시 한 달 전 '2024-11-01' 날짜여야 한다", () => {});
+it("주간 뷰에서 다음으로 navigate시 7일 후 '2024-10-08' 날짜로 지정이 된다", () => {
+  const { result } = renderHook(() => useCalendarView());
 
-it("월간 뷰에서 이전으로 navigate시 한 달 전 '2024-09-01' 날짜여야 한다", () => {});
+  act(() => {
+    result.current.setView('week');
+  });
 
-it("currentDate가 '2024-01-01' 변경되면 1월 휴일 '신정'으로 업데이트되어야 한다", async () => {});
+  act(() => {
+    result.current.navigate('next');
+  });
+
+  const date = new Date('2024-10-08');
+  assertDate(result.current.currentDate, date);
+});
+
+it("주간 뷰에서 이전으로 navigate시 7일 후 '2024-09-24' 날짜로 지정이 된다", () => {
+  const { result } = renderHook(() => useCalendarView());
+
+  act(() => {
+    result.current.setView('week');
+  });
+
+  act(() => {
+    result.current.navigate('prev');
+  });
+
+  const date = new Date('2024-09-24');
+  assertDate(result.current.currentDate, date);
+});
+
+it("월간 뷰에서 다음으로 navigate시 한 달 전 '2024-11-01' 날짜여야 한다", () => {
+  const { result } = renderHook(() => useCalendarView());
+
+  act(() => {
+    result.current.setView('month');
+  });
+
+  act(() => {
+    result.current.navigate('next');
+  });
+
+  const date = new Date('2024-11-01');
+  assertDate(result.current.currentDate, date);
+});
+
+it("월간 뷰에서 이전으로 navigate시 한 달 전 '2024-09-01' 날짜여야 한다", () => {
+  const { result } = renderHook(() => useCalendarView());
+
+  act(() => {
+    result.current.setView('month');
+  });
+
+  act(() => {
+    result.current.navigate('prev');
+  });
+
+  const date = new Date('2024-09-01');
+  assertDate(result.current.currentDate, date);
+});
+
+it("currentDate가 '2024-01-01' 변경되면 1월 휴일 '신정'으로 업데이트되어야 한다", async () => {
+  const { result } = renderHook(() => useCalendarView());
+
+  act(() => {
+    result.current.setCurrentDate(new Date('2024-01-01'));
+  });
+
+  expect(result.current.holidays).toMatchObject({
+    '2024-01-01': '신정',
+  });
+});
--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -1,14 +1,183 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
+import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { useSearch } from '../../hooks/useSearch.ts';
 import { Event } from '../../types.ts';
 
-it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {});
+describe('일정검색 test', () => {
+  //   const { result: eventResult } = renderHook(() => useEventOperations(false));
+  //   const { events } = eventResult.current;
 
-it('검색어에 맞는 이벤트만 필터링해야 한다', () => {});
+  const events: Event[] = [
+    {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2024-11-20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    },
+    {
+      id: '09702fb3-a478-40b3-905e-9ab3c8849dcd',
+      title: '점심 약속',
+      date: '2024-11-21',
+      startTime: '12:30',
+      endTime: '13:30',
+      description: '동료와 점심 식사',
+      location: '회사 근처 식당',
+      category: '개인',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    },
+    {
+      id: 'da3ca408-836a-4d98-b67a-ca389d07552b',
+      title: '프로젝트 마감',
+      date: '2024-11-25',
+      startTime: '09:00',
+      endTime: '18:00',
+      description: '분기별 프로젝트 마감',
+      location: '사무실',
+      category: '업무',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    },
+    {
+      id: 'dac62941-69e5-4ec0-98cc-24c2a79a7f81',
+      title: '생일 파티',
+      date: '2024-11-28',
+      startTime: '19:00',
+      endTime: '22:00',
+      description: '친구 생일 축하',
+      location: '친구 집',
+      category: '개인',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    },
+    {
+      id: '80d85368-b4a4-47b3-b959-25171d49371f',
+      title: '운동',
+      date: '2024-11-22',
+      startTime: '18:00',
+      endTime: '19:00',
+      description: '주간 운동',
+      location: '헬스장',
+      category: '개인',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    },
+  ];
 
-it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {});
+  it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', async () => {
+    // 준일 : 4번테스트와 충돌입니다. 왜죠?
+    const { result: searchResult } = renderHook(() => useSearch(events, new Date(), 'month'));
 
-it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {});
+    const { setSearchTerm, filteredEvents } = searchResult.current;
+    act(() => {
+      setSearchTerm('');
+    });
 
-it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {});
+    expect(filteredEvents).toEqual(events);
+  });
+
+  it('검색어에 맞는 이벤트만 필터링해야 한다', async () => {
+    const { result: searchResult } = renderHook(() => useSearch(events, new Date(), 'month'));
+
+    act(() => {
+      searchResult.current.setSearchTerm('헬스장');
+    });
+    const { filteredEvents } = searchResult.current;
+
+    await waitFor(() => {
+      filteredEvents.forEach((event: Event) => {
+        const searchTargets: EventKeys[] = ['title', 'description', 'location'];
+
+        const isFiltered = searchTargets.some((target: EventKeys) => event[target] === '헬스장');
+
+        expect(isFiltered).toBeTruthy();
+      });
+    });
+  });
+
+  it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', async () => {
+    const { result: searchResult } = renderHook(() => useSearch(events, new Date(), 'month'));
+
+    act(() => {
+      searchResult.current.setSearchTerm('헬스장');
+    });
+    const { filteredEvents } = searchResult.current;
+
+    await waitFor(() => {
+      filteredEvents.forEach((event: Event) => {
+        const searchTargets: EventKeys[] = ['title', 'description', 'location'];
+
+        const isFiltered = searchTargets.some((target: EventKeys) => event[target] === '헬스장');
+
+        expect(isFiltered).toBeTruthy();
+      });
+    });
+  });
+
+  it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {
+    const { result: searchMonthResult } = renderHook(() =>
+      useSearch(events, new Date('2024-11-01'), 'month')
+    );
+    const { filteredEvents } = searchMonthResult.current;
+    const expectedEvents = events.filter((event) => event.date.startsWith('2024-11'));
+    expect(filteredEvents).toEqual(expectedEvents);
+
+    const { result: searchWeekResult } = renderHook(() =>
+      useSearch(events, new Date('2024-11-18'), 'week')
+    );
+    const { filteredEvents: filteredWeekEvents } = searchWeekResult.current;
+    const expectedWeekEvents = events.filter((event) => {
+      const [year, month, day] = event.date.split('-').map(Number);
+      if (year === 2024 && month === 11 && day >= 17 && day <= 23) return event;
+    });
+
+    expect(filteredWeekEvents).toEqual(expectedWeekEvents);
+  });
+
+  it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
+    const { result: searchResult } = renderHook(() => useSearch(events, new Date(), 'month'));
+    act(() => {
+      searchResult.current.setSearchTerm('회의');
+    });
+
+    const isFiltered = searchResult.current.filteredEvents.every(
+      (event) => event.id === events[0].id
+    );
+
+    expect(isFiltered).toBeTruthy();
+
+    act(() => {
+      searchResult.current.setSearchTerm('점심');
+    });
+
+    const isLunchFiltered = searchResult.current.filteredEvents.every(
+      (event) => event.id === events[1].id
+    );
+
+    expect(isLunchFiltered).toBeTruthy();
+  });
+});
+
+type EventKeys = keyof Event;
